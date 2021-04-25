a71a32dc1dc048537d670740e7c0832c119ea339
Alana <alanayee07@gmail.com>
2021-04-25 15:20:17 -0700

    progress

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.use((req, res, next) => {
+ res.header('Access-Control-Allow-Origin', '*');
+ res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+ next();
+});
+
+module.exports = app;
